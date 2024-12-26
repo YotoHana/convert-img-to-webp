495c4468240b9bae54c41322d685d4f49d746ff9
Leonid Lugovoj <yotohana.dev@gmail.com>
2024-12-27 01:01:36 +0500

    create .exe file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from PIL import Image
 from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QSlider, QListWidget, QFileDialog
+from concurrent.futures import ThreadPoolExecutor
 from PyQt5.QtCore import Qt
+from PIL import Image
 import sys
 import os
 
@@ -11,33 +12,44 @@ def update_slider_lable(value):
     slider_label.setText("Качество: " + str(value))
 
 def open_file_dialog():
-    #options = QFileDialog.Options
-    files, _ = QFileDialog.getOpenFileNames(None, 'Choose files', '', "Image files (*.jpg *.jpeg *.png)")
+    files, _ = QFileDialog.getOpenFileNames(None, 'Choose files', '', "Image files (*.jpg *.jpeg *.png *.heif *.heic *.jfif *.bmp *.tiff *.tif *.pjpeg *.pjp)")
     if files:
         fileDialog.clear()
+        file_list.clear()
+        temp_file_list.clear()
         total_size = 0
         for file in files:
             fileDialog.addItem(file)
             file_list.append(file)
             total_size += os.path.getsize(file)
-        input_size_label.setText(f'Исходный размер: {total_size / 1024:.2f} KB')
+        input_size_label.setText(f'Исходный размер: {total_size / (1024 * 1024):.2f} МB')
         total_size = 0
-        for file in file_list:
-            convert_temp(file)
+        with ThreadPoolExecutor() as executor:
+            executor.map(convert_temp, file_list)
         for file in temp_file_list:
             total_size += os.path.getsize(file)
-        output_size_label.setText(f'Итоговый размер: {total_size / 1024:.2f} KB')
+        output_size_label.setText(f'Итоговый размер: {total_size / (1024 * 1024):.2f} МB')
+
+def clear_tmp():
+    for filename in os.listdir("./tmp/"):
+        file_path = os.path.join("./tmp/", filename)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(f'Ошибка при удалении файла {file_path}: {e}')
 
 def complete():
     file_list.clear()
     temp_file_list.clear()
     fileDialog.clear()
-    input_size_label.setText("Исходный размер: 0 KB")
-    output_size_label.setText("Итоговый размер: 0 KB")
+    input_size_label.setText("Исходный размер: 0 МB")
+    output_size_label.setText("Итоговый размер: 0 МB")
+    clear_tmp()
 
 def convert_files():
-    for file in file_list:
-        convert(file)
+    with ThreadPoolExecutor() as executor:
+        executor.map(convert, file_list)
     complete()
     
 
@@ -77,10 +89,10 @@ slider.valueChanged.connect(update_slider_lable)
 fileDialog = QListWidget()
 main_layout.addWidget(fileDialog)
 
-input_size_label = QLabel("Исходный размер: 0 KB")
+input_size_label = QLabel("Исходный размер: 0 МB")
 main_layout.addWidget(input_size_label)
 
-output_size_label = QLabel("Итоговый размер: 0 KB")
+output_size_label = QLabel("Итоговый размер: 0 МB")
 main_layout.addWidget(output_size_label)
 
 file_button = QPushButton("Выбрать файлы...")
@@ -95,3 +107,4 @@ window.setLayout(main_layout)
 
 window.show()
 app.exec_()
+clear_tmp()
